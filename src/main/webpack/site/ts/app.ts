0aaaf4481ea895e26fb389c10c150e55fd56fe48
Paul Myers <myersp63@gmail.com>
2022-02-22 22:04:00 -0500

    added v1, v2 queryall typescript sample

--- src/main/webpack/site/ts/app.ts
+++ src/main/webpack/site/ts/app.ts
@@ -1,1 +1,24 @@
 console.log('Hiya');
+
+
+const cards1 = document.querySelectorAll('.card.v1');
+cards1.forEach(function(card) {
+    const button = card.querySelector('.button');
+    button.addEventListener('click', () => {
+        card.querySelector<HTMLElement>('.added-to-cart').style.display = 'block';
+        console.log(card.children);
+        });
+});
+
+
+const cards2 = document.querySelectorAll('.card.v2');
+cards2.forEach(function(card) {
+    const button = card.querySelector('.button');
+    button.addEventListener('click', () => {
+        card.querySelector<HTMLElement>('.added-to-cart').style.display = 'block';
+        card.querySelector<HTMLElement>('.extra').style.display = 'block';
+        console.log(card.children);
+        });
+});
+
+
